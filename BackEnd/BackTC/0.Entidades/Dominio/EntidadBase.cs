66f897171048ff52630b9d1b72e949cc09ae7f87
Marchio Roque M <martinmarchio27@gmail.com>
2023-03-03 11:51:01 -0300

    .

---
+++ BackEnd/BackTC/0.Entidades/Dominio/EntidadBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0.Entidades.Dominio
+{
+    public class EntidadBase
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ BackEnd/BackTC/0.Entidades/Dominio/Tarea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0.Entidades.Dominio
+{
+    public class Tarea : EntidadBase
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set;}
+    }
+}
